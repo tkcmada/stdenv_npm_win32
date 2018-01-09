8b7203a0127d88307044d5b0d245a16cf3a5fcd6
Tokutaro Shimada <tokutaro@gmail.com>
2018-01-10 00:47:01 +0900

    installed all packages into vue_sample-x86.

---
+++ vue_sample-x86/node_modules/serverless/lib/plugins/remove/remove.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const BbPromise = require('bluebird');
+const userStats = require('../../utils/userStats');
+
+class Remove {
+  constructor(serverless) {
+    this.serverless = serverless;
+
+    this.commands = {
+      remove: {
+        usage: 'Remove Serverless service and all resources',
+        lifecycleEvents: [
+          'remove',
+        ],
+        options: {
+          stage: {
+            usage: 'Stage of the service',
+            shortcut: 's',
+          },
+          region: {
+            usage: 'Region of the service',
+            shortcut: 'r',
+          },
+          verbose: {
+            usage: 'Show all stack events during deployment',
+            shortcut: 'v',
+          },
+        },
+      },
+    };
+
+    this.hooks = {
+      'after:remove:remove': () => BbPromise.bind(this).then(this.track),
+    };
+  }
+
+  track() {
+    userStats.track('service_removed');
+    return BbPromise.resolve();
+  }
+}
+
+module.exports = Remove;
